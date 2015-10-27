b150db40b355ce5a362686086562586b495fb263
Willem Ellis <willem.ellis@gmail.com>
2015-10-27 16:15:40 -0500

    forgot to update dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,19 +26,7 @@
           <td class="vcenter">{{ date('F d, Y', strtotime($invoice->due_date)) }}</td>
           <td class="vcenter">{{ $invoice->total }}</td>
           <td class="vcenter">
-            <form method="post" action="{{ route('dashboard.clients.invoices.update', [$invoice->client->id, $invoice->id]) }}">
-              <a class="btn btn-info view-btn" href="#" data-invoice="{{ $invoice->id }}" data-toggle="modal" data-target="preview_modal">
-                <span class="glyphicon glyphicon-eye-open"></span>
-                View
-              </a>
-              {!! method_field('put') !!}
-              {!! csrf_field() !!}
-              <input type="hidden" name="paid" value="1">
-              <button type="submit" class="btn btn-success" onclick="return confirm('Are you sure you want to mark this invoice as paid? This cannot be undone.');">
-                <span class="glyphicon glyphicon-usd"></span>
-                Paid
-              </button>
-            </form>
+            @include('invoices.partials.actions')
           </td>
         </tr>
         @endforeach
